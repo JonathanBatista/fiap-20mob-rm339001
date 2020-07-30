8878eaeabc3862c1ad9e9c49210310d2627c11df
Jonathan Batista <jonathan.batistasilva@outlook.com>
2020-07-30 19:38:26 -0300

    segundo commit

--- app/application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/"
-
-if __name__ == '__main__':
-    application.run()

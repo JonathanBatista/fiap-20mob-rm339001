2edf4d9651cdb844d069b9792a68342be750dfa9
Jonathan Batista da Silva <jonathan.batistasilva@outlook.com>
2020-08-07 09:58:58 -0300

    .net web solution

---
+++ Fiap.20mob.Cloud.Api/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap._20mob.Cloud.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return ViewResult(@"
+<!doctype html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>FIAP</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+                
+</body>
+</html>
+            ");
+        }
+    }
+}
